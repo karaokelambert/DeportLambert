51c85d51439f8190a916fccf48e8bed527c5f266
LAMBERTJ <lambertj@sportsclub360.com>
2026-08-29 18:06:09 -0400

    Configurar basePath y assetPrefix /DeportLambert con .nojekyll para GitHub Pages

--- DeportLambert-main/next.config.js
+++ DeportLambert-main/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/DeportLambert',
+  assetPrefix: '/DeportLambert',
   trailingSlash: true,
   reactStrictMode: true,
   images: {
